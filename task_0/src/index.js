7b3b8f73ee56a597a8754a4dbad8da212dc56644
Okibe Onmeje <Okibeogomola@gmail.com>
2024-08-01 15:22:56 +0100

    add init file

---
+++ task_0/src/index.js
@@ -0,0 +1,10 @@
+const $ = require("jquery");
+
+let str = `
+	<p>Holberton Dashboard</p>
+	<p>Dashboard data for the students</p>
+  <p>Copyright - Holberton School</p>
+	`
+$("body").append(
+	str
+);
