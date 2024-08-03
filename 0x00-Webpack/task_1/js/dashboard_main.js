fd1cf4fa71862a426995adb57264b447e5bfb96b
Okibe Onmeje <Okibeogomola@gmail.com>
2024-08-03 12:01:49 +0100

    add counter and paragraph to page

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,19 @@
+import $ from "jquery";
+import _ from "lodash"
+
+let counter = 0;
+$(document).ready(function() {
+	$( "body" ).append("<p>Holberton Dashboard</p>");
+	$( "body" ).append("<p>Dashboard data for the students</p>");
+	$( "body" ).append("<button>Click here to get started</button>");
+	$( "body" ).append("<p id='count'></p>");
+	$( "body" ).append("<p>Copyright - Holberton School</p>");
+
+	$( "button" ).on("click", _.debounce(updateCounter, 150));
+});
+
+function updateCounter() {
+	counter += 1;
+	$( "#count" ).text(`{${counter}} clicks on the button`);
+}
+
