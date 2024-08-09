99f9a2e599ba48ff8be0dc1654745cdec123982f
Okibe Onmeje <Okibeogomola@gmail.com>
2024-08-09 17:20:00 +0100

    create notifications elements

---
+++ 0x01-react_intro/task_1/dashboard/src/Notifications.js
@@ -0,0 +1,10 @@
+// import React from 'React';
+import './Notifications.css';
+
+export function Notifications() {
+	return (
+		<div className="notify">
+			<p>Here is the list of notifications</p>
+		</div>
+	)
+}
