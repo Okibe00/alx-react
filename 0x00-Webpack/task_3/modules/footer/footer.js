1a6cd2b57cebdb2920a473ab13431bad8fc366d4
Okibe Onmeje <Okibeogomola@gmail.com>
2024-08-11 18:21:34 +0100

    Updates

--- 0x00-Webpack/task_3/modules/footer/footer.js
+++
@@ -1,8 +0,0 @@
-import "./footer.css"
-import $ from "jquery"
-
-
-$(document).ready(function() {
-	$( "body" ).append("<p class='footer'>Copyright - Holberton School</p>");
-
-});
--- 0x00-Webpack/task_3/modules/header/header.js
+++
@@ -1,7 +0,0 @@
-import $ from "jquery"
-import "./header.css"
-
-$(document).ready(function() {
-	$("body").append("<h1>Holberton Dashboard</p>");
-	console.log("Init header");
-});
