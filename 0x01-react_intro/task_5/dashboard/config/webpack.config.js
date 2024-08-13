8cb47f4c607b94b9eecd721eb36356e90bfad2b4
Okibe Onmeje <Okibeogomola@gmail.com>
2024-08-13 10:45:17 +0100

    correct directory path

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -2,11 +2,11 @@ const path = require('path');
 
 module.exports = {
 	mode: 'development',
-	entry: path.resolve(__dirname,'src/index.js'),
+	entry: path.resolve(__dirname,'../src/index.js'),
 	devtool: 'inline-source-map',
 	output: {
 		filename: 'bundle.js',
-		path: path.resolve(__dirname,'dist')
+		path: path.resolve(__dirname, '../dist')
 	},
 	devServer: {
 		static: {
