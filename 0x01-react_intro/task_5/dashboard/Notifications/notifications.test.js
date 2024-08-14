94c5e1ceae1e4367f3f531b16e7dcdef72d5e9b4
Okibe Onmeje <Okibeogomola@gmail.com>
2024-08-14 12:40:55 +0100

    created test for app

---
+++ 0x01-react_intro/task_5/dashboard/Notifications/notifications.test.js
@@ -0,0 +1,7 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import { Notifications } from './Notifications.js';
+
+test("test notfication component", () => {
+	render(<Notifications />);
+})
--- 0x01-react_intro/task_5/dashboard/utils/utils.test.js
+++ 0x01-react_intro/task_5/dashboard/utils/utils.test.js
@@ -1,4 +1,4 @@
-import { getFullYear, getFooterCopy, getLatestNotification } from './utils.js'
+import { getFullYear, getFooterCopy, getLatestNotification } from './utils.js';
 import '@testing-library/react';
 
 	test("getFullYear()", () =>{
