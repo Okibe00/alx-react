c7738bed1059b91f5d02624b88be1ad3d74d40eb
Okibe Onmeje <Okibeogomola@gmail.com>
2024-09-21 22:32:59 +0100

    add HOC component
    - Log to the console Component NAME_OF_THE_WRAPPED_COMPONENT is mounted on componentDidMount()
    - Log to the console Component NAME_OF_THE_WRAPPED_COMPONENT is going to unmount on componentWillUnmount()
    - Modify displayName of the HOC to WithLogging(NAME_OF_THE_WRAPPED_COMPONENT) in the React Chrome Extension or for debugging
    -Default to Component when no name for wrapped component

---
+++ 0x03-React_component/task_4/dashboard/src/HOC/WithLogging.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default const WithLogging = (WrappedComp) => {
+  const enhancedClass = class extends React.Component {
+    componentDidMount() {
+      console.log(`Component ${WrappedComp.name} is mounted`)
+    }
+    componentWillUnmount(){
+      console.log(`Component ${WrappedComp.name} is going to unmount`)
+    }
+    render() {
+      return (<WrappedComp  {...this.props}/> )
+    }
+  }
+  enhancedClass.displayName = `WithLogging(${WrappedComp.name || WrappedComp.displayName || 'Component' })`
+  return enhancedClass;
+};
