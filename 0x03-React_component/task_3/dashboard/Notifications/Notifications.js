9d1d3f61dd5a66b37343e09a75ced27f2910ef0d
Okibe Onmeje <Okibeogomola@gmail.com>
2024-09-21 13:33:17 +0100

    copy task_2 int task_3
    - add BodySection component

---
+++ 0x03-React_component/task_3/dashboard/Notifications/Notifications.js
@@ -0,0 +1,64 @@
+import React from "react";
+import "./Notifications.css";
+import { IoIosClose } from "react-icons/io";
+import { getLatestNotification } from "../utils/utils.js";
+import NotificationItem from "./NotificationItem.js";
+import PropTypes from "prop-types";
+import { NotificationItemShape } from "./NotificationItemShape.js";
+
+export class Notifications extends React.Component {
+  markAsRead = (id) =>
+    console.log(`Notification ${id} has been marked as read`);
+
+  render() {
+    return (
+      <div className="notif-container">
+        <div className="menueItem">Your notifications</div>
+        {this.props.displayDrawer && (
+          <div className="notify">
+            {this.props.displayDrawer && (
+              <p>Here is the list of notifications</p>
+            )}
+            <button
+              aria-label="Close"
+              onClick={() => console.log("Close button has been clicked")}
+            >
+              <IoIosClose />
+            </button>
+            <ul>
+              {!this.props.listNotifications.length ? (
+                <NotificationItem
+                  value={"No new Notification for now"}
+                  type={"default"}
+                />
+              ) : (
+                this.props.listNotifications.map((obj) => {
+                  return (
+                    <NotificationItem
+                      markAsRead={this.markAsRead}
+                      key={obj.id}
+                      value={obj.value}
+                      type={obj.type}
+                      html={obj.html}
+                      key_id={obj.id}
+                    />
+                  );
+                })
+              )}
+            </ul>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+Notifications.propTypes = {
+  displayDrawer: PropTypes.bool,
+  listNotifications: PropTypes.arrayOf(NotificationItemShape),
+};
+
+Notifications.defaultProps = {
+  displayDrawer: false,
+  listNotifications: [],
+};
---
+++ 0x03-React_component/task_3/dashboard/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Notifications } from "../Notifications/Notifications.js";
+import App from "../App/App.js";
+import BodySection from "./BodySection/BodySection";
+const dom = document.querySelector("#root");
+const root = createRoot(dom);
+
+const logOut = () => {};
+root.render(
+  <div>
+    <App Isloggedin={true} logOut={logOut} />
+  </div>
+);
