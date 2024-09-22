ff030f124f48ae0569fbc93da2cf7c992d08b3fb
Okibe Onmeje <Okibeogomola@gmail.com>
2024-09-22 16:07:50 +0100

    add HOC component
    - add test for HOC component

--- 0x03-React_component/task_4/dashboard/src/HOC/WithLogging.js
+++ 0x03-React_component/task_4/dashboard/src/HOC/WithLogging.js
@@ -1,17 +1,20 @@
 import React from "react";
 
-export default const WithLogging = (WrappedComp) => {
+const WithLogging = (WrappedComp) => {
   const enhancedClass = class extends React.Component {
     componentDidMount() {
-      console.log(`Component ${WrappedComp.name} is mounted`)
+      console.log(`Component ${WrappedComp.name} is mounted`);
     }
-    componentWillUnmount(){
-      console.log(`Component ${WrappedComp.name} is going to unmount`)
+    componentWillUnmount() {
+      console.log(`Component ${WrappedComp.name} is going to unmount`);
     }
     render() {
-      return (<WrappedComp  {...this.props}/> )
+      return <WrappedComp {...this.props} />;
     }
-  }
-  enhancedClass.displayName = `WithLogging(${WrappedComp.name || WrappedComp.displayName || 'Component' })`
+  };
+  enhancedClass.displayName = `WithLogging(${
+    WrappedComp.name || WrappedComp.displayName || "Component"
+  })`;
   return enhancedClass;
 };
+export default WithLogging;
--- 0x03-React_component/task_4/dashboard/src/Login/Login.js
+++ 0x03-React_component/task_4/dashboard/src/Login/Login.js
@@ -1,18 +1,15 @@
-import React from 'react';
-import './Login.css';
+import React from "react";
+import "./Login.css";
 
-
-export default function Login () {
+export default function Login() {
   return (
-  <div className="App-body">
-    <p>Login to access the full dashboard</p>
-    <label htmlFor="email">
-      Email:
-    </label>
-    <input id='email' type="email" />
-    <label htmlFor="password">Password</label>
-    <input id='password' type="password" />
-    <button>Ok</button>
-  </div>
-  )
+    <div className="App-body">
+      <p>Login to access the full dashboard</p>
+      <label htmlFor="email">Email:</label>
+      <input id="email" type="email" />
+      <label htmlFor="password">Password</label>
+      <input id="password" type="password" />
+      <button>Ok</button>
+    </div>
+  );
 }
