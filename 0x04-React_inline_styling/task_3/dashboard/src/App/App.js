0674a1bb0f3e06e8e4d289a438300f34be50dcdf
Okibe Onmeje <Okibeogomola@gmail.com>
2024-09-26 12:28:43 +0100

    add response for design for login screen

---
+++ 0x04-React_inline_styling/task_3/dashboard/src/App/App.js
@@ -0,0 +1,99 @@
+import React from "react";
+import Header from "../Header/Header.js";
+import Footer from "../Footer/Footer.js";
+import Login from "../Login/Login.js";
+import { Notifications } from "../Notifications/Notifications.js";
+import CourseList from "../CourseList/CourseList.js";
+import PropTypes from "prop-types";
+import { getLatestNotification } from "../utils/utils.js";
+import BodySectionWithMarginBottom from "../BodySection/BodySectionWithMarginBottom.js";
+import BodySection from "../BodySection/BodySection.js";
+
+class App extends React.Component {
+  listCourses = [
+    {
+      id: 1,
+      name: "ES6",
+      credit: 60,
+    },
+    {
+      id: 2,
+      name: "Webpack",
+      credit: 20,
+    },
+    {
+      id: 3,
+      name: "React",
+      credit: 40,
+    },
+  ];
+
+  listNotifications = [
+    {
+      id: 1,
+      value: "New course available",
+      type: "default",
+    },
+    {
+      id: 2,
+      value: "New resume available",
+      type: "",
+    },
+    {
+      id: 3,
+      type: "urgent",
+      value: getLatestNotification(false),
+    },
+  ];
+
+  handleLogOut = (e) => {
+    if (e.key === "h" && e.ctrlKey) {
+      alert("Logging you out");
+      this.props.logOut();
+    }
+  };
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleLogOut);
+  }
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleLogOut);
+  }
+  render() {
+    return (
+      <div className="App">
+        <Notifications
+          displayDrawer={true}
+          listNotifications={this.listNotifications}
+        />
+        <Header />
+        {this.props.Isloggedin ? (
+          <BodySectionWithMarginBottom title={"Course list"}>
+            <CourseList listCourses={this.listCourses} />
+
+          </BodySectionWithMarginBottom>
+        ) : (
+          <BodySectionWithMarginBottom title={"Log in to continue"}>
+            <Login />
+            <BodySection title={"News from the School"}>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ante lorem, vehicula eget elementum ac, vulputate sit amet quam. Donec quis placerat elit. Ut ut porta lorem. Fusce vel nulla congue, porttitor enim vitae, condimentum tellus. Morbi lorem ante, pretium sit amet convallis nec, convallis et eros. Integer purus erat, commodo sit amet vulputate sit amet, mattis et mi. Duis posuere hendrerit ipsum, et finibus ex cursus sit amet.
+              </p>
+              <p>
+              Nunc eu quam viverra, consectetur diam a, egestas quam. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque efficitur faucibus ullamcorper. Curabitur in neque interdum, commodo neque eu, molestie odio. Integer malesuada orci a enim molestie posuere vitae at urna. Quisque ac sollicitudin magna. Suspendisse risus orci, ornare eu facilisis at, mollis eu est. Pellentesque egestas et velit at vehicula. Nulla sodales et ipsum nec pellentesque.
+              </p>
+            </BodySection>
+          </BodySectionWithMarginBottom>
+        )}
+        <Footer />
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  Isloggedin: PropTypes.bool,
+  logOut: PropTypes.func,
+};
+App.defaultPropTypes = {
+  logOut: function () {},
+};
+export default App;
---
+++ 0x04-React_inline_styling/task_3/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from "prop-types";
+import {StyleSheet, css} from 'aphrodite';
+
+const style = StyleSheet.create({
+  urgent: {color: 'red'},
+  default: {color: 'darkblue'}
+})
+const NotificationItem = React.memo(({type = 'default', value, html, markAsRead, key_id}) => {
+  const display = html ? { __html: html } : { __html: `<data>${value}</data>` };
+  return (
+    <li
+      data-notification-type={type}
+      dangerouslySetInnerHTML={display}
+      onClick={() => markAsRead(key_id)}
+      className={type === 'urgent' ? css(style.urgent): type === 'default' ? css(style.default): ''}
+    ></li>
+  );
+});
+
+NotificationItem.propTypes = {
+  value: PropTypes.string,
+  type: PropTypes.string,
+  html: PropTypes.shape({ __html: PropTypes.string }),
+  markAsRead: PropTypes.func,
+};
+
+// NotificationItem.defaultProps = {
+//   type: "default",
+// };
+export default NotificationItem;
