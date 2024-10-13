2175d966382c060da6affbd3d59a8074bb27febc
Okibe Onmeje <Okibeogomola@gmail.com>
2024-10-13 15:35:58 +0100

    use merge and concat on immutable objects

---
+++ 0x06-React_Immutable/5-merge.js
@@ -0,0 +1,27 @@
+const { Map, List } = require('immutable');
+
+function concatElements(page1, page2) {
+  return (List(page1).concat(page2));
+}
+
+function mergeElements(page1, page2) {
+  const page1Keys = Object.keys(page1);
+  const page2Keys = Object.keys(page2);
+  let flag = true;
+  if (page1Keys.length === page2Keys.length) {
+    for (const key in page1Keys) {
+      if (page1[key] !== page2[key]) {
+        flag = false;
+      }
+    }
+  }
+  if (!flag) {
+    return Map(page2);
+  }
+  return Map(page1).merge(page2);
+}
+
+module.exports = {
+  concatElements,
+  mergeElements,
+};
