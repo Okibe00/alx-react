560bed20b401d2f93626ad488fda57967162935a
Okibe Onmeje <Okibeogomola@gmail.com>
2024-10-14 10:28:29 +0100

    convert to plain js object after merge

--- 0x06-React_Immutable/6-deeply.js
+++ 0x06-React_Immutable/6-deeply.js
@@ -1,7 +1,11 @@
-const { Map, List } = require('immutable');
+const { Map } = require('immutable');
 
 function mergeDeeplyElements(page1, page2) {
-  return List(Map(page1).mergeDeep(page2).valueSeq());
+  const m1 = Map(page1);
+  const m2 = Map(page2);
+
+  const merged = m1.mergeDeep(m2);
+  return (merged.toJS());
 }
 
 module.exports = {
