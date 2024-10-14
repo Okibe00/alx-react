65cc487793323a0ca42d01cd5f09c057e7640a51
Okibe Onmeje <Okibeogomola@gmail.com>
2024-10-14 18:35:26 +0100

    refactor to return immutable list

--- 0x06-React_Immutable/6-deeply.js
+++ 0x06-React_Immutable/6-deeply.js
@@ -5,9 +5,27 @@ function mergeDeeplyElements(page1, page2) {
   const m2 = Map(page2);
 
   const merged = m1.mergeDeep(m2);
-  return (merged.toJS());
+  return (merged);
 }
 
 module.exports = {
   mergeDeeplyElements,
 };
+
+// const { Map, List } = require('immutable');
+
+// function mergeDeeplyElements(page1, page2) {
+//   // Convert both objects into Immutable Maps
+//   const map1 = Map(page1);
+//   const map2 = Map(page2);
+
+//   // Deeply merge the two maps
+//   const mergedMap = map1.mergeDeep(map2);
+
+//   // Extract the values of the merged map and convert them to an Immutable List
+//   return List(mergedMap.valueSeq());
+// }
+
+// module.exports = {
+//   mergeDeeplyElements,
+// };
