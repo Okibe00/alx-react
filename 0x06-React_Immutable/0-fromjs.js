8e572898fb93bb7078bc5e5c357f24c1df4d785e
Okibe Onmeje <Okibeogomola@gmail.com>
2024-10-16 14:54:55 +0100

    use es6 import syntax

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,13 +1,4 @@
-/**
- * @module - creates an immutable Map object
- */
-const { fromJS } = require('immutable');
-
-/**
- * creates an immutable mao object
- * @param { Object{} }: object
- * @Return { Object{} }: immutable Map object
- */
+import { fromJS } from 'immutable';
 
 function getImmutableObject(object) {
   return fromJS(object);
