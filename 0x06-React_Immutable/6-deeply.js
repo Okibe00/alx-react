fbb0cffe0fdc736b1e330cbd880b1e501bdd0159
Okibe Onmeje <Okibeogomola@gmail.com>
2024-10-16 10:00:29 +0100

    fix convert to list

--- 0x06-React_Immutable/6-deeply.js
+++ 0x06-React_Immutable/6-deeply.js
@@ -1,31 +1,14 @@
-const { Map } = require('immutable');
+const { Map, List } = require('immutable');
 
 function mergeDeeplyElements(page1, page2) {
-  const m1 = Map(page1);
-  const m2 = Map(page2);
-
-  const merged = m1.mergeDeep(m2);
-  return (merged);
+  const firstPage = Map(page1);
+  const secondPage = Map(page2);
+  const mergedPage = firstPage.mergeDeep(secondPage);
+  // return List(mergedPage);
+  const lis = List([mergedPage.toJS()]);
+  return lis;
 }
 
 module.exports = {
   mergeDeeplyElements,
 };
-
-// const { Map, List } = require('immutable');
-
-// function mergeDeeplyElements(page1, page2) {
-//   // Convert both objects into Immutable Maps
-//   const map1 = Map(page1);
-//   const map2 = Map(page2);
-
-//   // Deeply merge the two maps
-//   const mergedMap = map1.mergeDeep(map2);
-
-//   // Extract the values of the merged map and convert them to an Immutable List
-//   return List(mergedMap.valueSeq());
-// }
-
-// module.exports = {
-//   mergeDeeplyElements,
-// };
