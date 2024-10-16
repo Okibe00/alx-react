648e0f5728b6cb65c84bf2f6ca4f88c16930fe58
Okibe Onmeje <Okibeogomola@gmail.com>
2024-10-16 16:36:50 +0100

    change import syntax to es6

--- 0x06-React_Immutable/1-map.js
+++ 0x06-React_Immutable/1-map.js
@@ -1,7 +1,4 @@
-/**
- * @module - utils for working for creating immunatable objects
- */
-const { Map } = require('immutable');
+import { Map } from 'immutable';
 
 function getImmutableObject(obj) {
   return Map(obj);
