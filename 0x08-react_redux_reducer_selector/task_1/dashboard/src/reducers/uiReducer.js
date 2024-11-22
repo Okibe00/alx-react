7542dbbaa8c17a924a6e2bbf316601244970c801
okibe onmeje <okibeogomola@gmail.com>
2024-11-22 23:17:45 +0100

    feat(redux, uiReducer): reducer now returns immutable object

--- 0x08-react_redux_reducer_selector/task_1/dashboard/src/reducers/uiReducer.js
+++ 0x08-react_redux_reducer_selector/task_1/dashboard/src/reducers/uiReducer.js
@@ -3,12 +3,13 @@
  * @description - app UI reducer
  */
 import { LOGIN, LOGIN_FAILURE, LOGOUT, DISPLAY_NOTIFICATION_DRAWER, HIDE_NOTIFICATION_DRAWER, LOGIN_SUCCESS } from '../actions/uiActionTypes';
+import { Map } from 'immutable';
 
-let initialState = {
+let initialState = Map({
     isNotificationDrawerVisible: false,
     isUserLoggedIn: false,
     user: {}
-  }
+  })
 
 
 /**
@@ -19,32 +20,15 @@ let initialState = {
 export function uiReducer(state = initialState, action = {}) {
   switch (action.type) {
     case 'LOGOUT':
-      return {
-        ...state,
-        isUserLoggedIn: false
-      };
-    case 'LOGIN':
-      break;
+      return state.set('isUserLoggedIn', false);
     case 'LOGIN_FAILURE':
-      return {
-        ...state,
-        isUserLoggedIn:false
-      };
+      return state.set('isUserLoggedIn', false);
     case 'LOGIN_SUCCESS':
-      return {
-        ...state,
-        isUserLoggedIn: true
-      };
+      return state.set('isUserLoggedIn', true);
     case 'DISPLAY_NOTIFICATION_DRAWER':
-      return {
-        ...state,
-        isNotificationDrawerVisible: true
-      };
+      return state.set('isNotificationDrawerVisible', true);
     case 'HIDE_NOTIFICATION_DRAWER':
-      return {
-        ...state,
-        isNotificationDrawerVisible: false
-      };
+      return state.set('isNotificationDrawerVisible', false);
     default:
       return state;
   }
