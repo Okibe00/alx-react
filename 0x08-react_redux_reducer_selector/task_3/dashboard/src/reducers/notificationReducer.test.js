cffa3edf48c9ae41a985928122c391b7d2c64395
okibe onmeje <okibeogomola@gmail.com>
2024-11-30 11:33:21 +0100

    test(notificationReducer): add more notificationReducer test

--- 0x08-react_redux_reducer_selector/task_3/dashboard/src/reducers/notificationReducer.test.js
+++ 0x08-react_redux_reducer_selector/task_3/dashboard/src/reducers/notificationReducer.test.js
@@ -34,10 +34,59 @@ describe('Notification Reducer', () => {
         filter: 'DEFAULT'
       }
     const state = notificationReducer(initialState, action);
-    console.log(state);
+    // console.log(state);
     state.notifications.forEach(notification => {
       expect(notification.isRead).toBe(false);
     })
     expect(state.filter).toBe('DEFAULT');
+  })
+  test('MARK_AS_READ, should set isRead to true', () => {
+    const  initialState = {
+      filter: "DEFAULT",
+      notifications: [
+        {
+          id: 1,
+          isRead: false,
+          type: "default",
+          value: "New course available"
+        },
+        {
+          id: 2,
+          isRead: true,
+          type: "urgent",
+          value: "New resume available"
+        },
+        {
+          id: 3,
+          isRead: false,
+          type: "urgent",
+          value: "New data available"
+        }
+      ]
+    }
+
+    const action = {
+      type: 'MARK_AS_READ',
+      index: 1
+    }
+
+    const state = notificationReducer(initialState, action);
+    state.notifications.forEach(notification => {
+      if (notification.id == action.index) {
+        expect(notification.isRead).toBe(true);
+      }
+    })
+  });
+  test('SET_FILTER_TYPE should set change filter key', () => {
+    const initialState = {
+      filter: 'DEFAULT',
+      notifications: []
+    }
+    const action = {
+      type: 'SET_TYPE_FILTER',
+      filter: 'URGENT'
+    }
+    const state = notificationReducer(initialState, action);
+    expect(state.filter).toBe(action.filter);
   })
 })
