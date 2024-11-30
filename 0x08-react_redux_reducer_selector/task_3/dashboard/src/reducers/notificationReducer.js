e31b8cce6019ec2d44ca8c38ef207677aa22f5bf
okibe onmeje <okibeogomola@gmail.com>
2024-11-30 10:49:10 +0100

    feat(redux, notificationReducer): implement notification reducer

---
+++ 0x08-react_redux_reducer_selector/task_3/dashboard/src/reducers/notificationReducer.js
@@ -0,0 +1,43 @@
+/**
+ * @module - notificationReducer
+ * @description - updates the notifiation slice of state
+ */
+
+
+const initialState = {
+  notitifications: [],
+  filter: 'DEFAIULT'
+}
+
+/**
+ * 
+ * @param {object} initialState - the state object
+ * @param {object} action - hold the details of what changed 
+ * @returns new state object 
+ */
+export const notificationReducer = (initialState, action) => {
+  switch(action.type) {
+    case 'FETCH_NOTIFICATIONS_SUCCESS':
+      var newNotifications = {...initialState}
+      newNotifications.notifications.forEach(notification => {
+        notification.isRead = false;
+      })
+      return newNotifications;
+    case 'MARK_AS_READ':
+      var newNotifications = {...initialState}
+      return newNotifications.notifications.map(notification => {
+        if (notification.id === action.index) {
+          notification.isRead = true;
+          return notification;
+        } else {
+          return notification;
+        }
+      });
+    case 'SET_TYPE_FILTER':
+      var newNotifications = {...initialState};
+      newNotifications.filter = action.filter;
+      return newNotifications;   
+    default:
+      return initialState;
+  }
+}
