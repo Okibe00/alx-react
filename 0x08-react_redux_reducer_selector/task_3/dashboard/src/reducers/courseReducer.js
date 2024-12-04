5a6441d3d9a64f3ba137ce8ad4afc1b00b7cedbd
okibe onmeje <okibeogomola@gmail.com>
2024-12-04 01:46:28 +0100

    add(courseReducer): use immutable objects

--- 0x08-react_redux_reducer_selector/task_3/dashboard/src/reducers/courseReducer.js
+++ 0x08-react_redux_reducer_selector/task_3/dashboard/src/reducers/courseReducer.js
@@ -1,46 +1,42 @@
 /**
  * @module courseReducer
  * @description contains a reducer for course state
- * 
+ *
  */
-import { List, Map } from 'immutable';
-import { FETCH_COURSE_SUCCESS } from '../actions/courseActionTypes';
+import { Map, setIn, merge } from "immutable";
+import { FETCH_COURSE_SUCCESS } from "../actions/courseActionTypes";
+import { coursesNormalizer } from "../schema/courses";
 
-const initialState = [];
+const initialState = Map();
 /**
  * @description - updates the course state
  * @param {list} initialState - state variable
  * @param {object} action - an object of instruction of what has changed/happened
  * @returns {object} new state object
  */
-export function courseReducer(initialState, action) {
-
-  switch(action.type) {
-    case 'FETCH_COURSE_SUCCESS':
-      const actionData = action.data.map(obj => {
-        obj.isSelected = false
+export function courseReducer(initialState = initialState, action) {
+  switch (action.type) {
+    case "FETCH_COURSE_SUCCESS":
+      const actionData = action.data.map((obj) => {
+        obj.isSelected = false;
         return obj;
       });
-      return [...initialState, ...actionData];
-      case 'SELECT_COURSE':
-        return initialState.map(obj => {
-          if (obj.id === action.index) {
-            obj.isSelected = true;
-            return obj;
-          } else {
-            return obj;
-          }
-        })
-        case 'UNSELECT_COURSE':
-          initialState.map(obj => {
-            if (obj.id === action.index) {
-              obj.isSelected = false;
-              return obj;
-            } else {
-              return obj;
-            }
-          });
-        default:
-          return initialState;
+      return initialState.merge(coursesNormalizer(actionData));
+    case "SELECT_COURSE":
+      var newState = Map(initialState);
+      return setIn(
+        newState,
+        ["entities", "courses", `${action.index}`, "isSelected"],
+        true
+      );
+    case "UNSELECT_COURSE":
+      var newState = Map(initialState);
+      return setIn(
+        newState,
+        ["entities", "courses", `${action.index}`, "isSelected"],
+        false
+      );
+    default:
+      return initialState;
   }
 }
