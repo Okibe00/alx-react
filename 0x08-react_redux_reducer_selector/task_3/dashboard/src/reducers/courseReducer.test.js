6a3525b93d2cbc4c3c8560b3530a50af1cc6c23d
okibe onmeje <okibeogomola@gmail.com>
2024-12-04 01:48:05 +0100

    test(courseReducer): courseRducer for immutable behaviour

--- 0x08-react_redux_reducer_selector/task_3/dashboard/src/reducers/courseReducer.test.js
+++ 0x08-react_redux_reducer_selector/task_3/dashboard/src/reducers/courseReducer.test.js
@@ -3,66 +3,75 @@
  * @description - test suite for course reducer
  */
 
+import { coursesNormalizer } from "../schema/courses";
 import { courseReducer } from "./courseReducer";
-  const data = [
-    {
-      id: 1,
-      name: "ES6",
-      isSelected: false,
-      credit: 60
-    },
-    {
-      id: 2,
-      name: "Webpack",
-      isSelected: true,
-      credit: 20
-    },
-    {
-      id: 3,
-      name: "React",
-      isSelected: false,
-      credit: 40
-    }
-  ]
-
-describe('course reducer', () => {
-  
-  test('FETCH_COURSE_SUCCESS should return the data passed', () => {
+import { Map } from "immutable";
+const data = [
+  {
+    id: 1,
+    name: "ES6",
+    isSelected: false,
+    credit: 60,
+  },
+  {
+    id: 2,
+    name: "Webpack",
+    isSelected: true,
+    credit: 20,
+  },
+  {
+    id: 3,
+    name: "React",
+    isSelected: false,
+    credit: 40,
+  },
+];
+const normalizedData = coursesNormalizer(data);
+// console.log(normalizedData.entities);
+describe("course reducer", () => {
+  test("FETCH_COURSE_SUCCESS should return the data passed", () => {
     const action = {
-      type: 'FETCH_COURSE_SUCCESS',
-      data: data
-    }
-    const initialState = []
+      type: "FETCH_COURSE_SUCCESS",
+      data: data,
+    };
+    const initialState = Map();
     const updatedState = courseReducer(initialState, action);
-    expect(updatedState.length).toBe(3);
-    updatedState.forEach(obj => {
-      expect(obj.isSelected).toBeFalsy();
-    })
-  })
-
-  test('SELECT_COURSE should set the isSelected to true', () => {
-    const action = {
-      type: 'SELECT_COURSE',
-      index: 2
+    const jsObject = updatedState.toJS();
+    const numOfCourses = Object.keys(jsObject.entities.courses).length;
+    const courses = jsObject.entities.courses;
+    expect(numOfCourses).toBe(3);
+    // console.log(jsObject.entities.courses);
+    for (let obj in courses) {
+      expect(courses[obj].isSelected).toBe(false);
+      // console.log(courses[obj]);
     }
-    const updatedState = courseReducer(data, action);
-    updatedState.forEach(obj => {
-      if (obj.id == 2) {
-        expect(obj.isSelected).toBe(true);
-      }
-    })
-  })
-
-  test('UNSELECT_COURSE should set the isSelected to false', () => {
-    const action = {
-      type: 'UNSELECT_COURSE',
-      index: 2
-    }
-    const updatedState = courseReducer(data, action);
-    updatedState.forEach(obj => {
-      if (obj.id == 2) {
-        expect(obj.isSelected).toBe(false);
-      }
-    })
   });
-})
+  test("SELECT_COURSE should set the isSelected to true", () => {
+    const action = {
+      type: "SELECT_COURSE",
+      index: 1,
+    };
+    const mapState = courseReducer(normalizedData, action);
+    const jsObject = mapState.toJS();
+    expect(
+      jsObject
+      .entities
+      .courses[action.index]
+      .isSelected
+    ).toBe(true);
+  });
+
+  test("UNSELECT_COURSE should set the isSelected to false", () => {
+    const action = {
+      type: "UNSELECT_COURSE",
+      index: 2,
+    };
+    const mapState = courseReducer(normalizedData, action);
+    const jsObject = mapState.toJS();
+    expect(
+      jsObject
+      .entities
+      .courses[action.index]
+      .isSelected).toBe(false);
+  });
+});
