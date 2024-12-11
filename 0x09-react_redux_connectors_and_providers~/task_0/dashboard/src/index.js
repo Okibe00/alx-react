b2cd3c684288cee3e712e36ceb22db860db7d71f
okibe onmeje <okibeogomola@gmail.com>
2024-12-11 11:05:05 +0100

    add(index.js, redux): redux store

--- 0x09-react_redux_connectors_and_providers~/task_0/dashboard/src/index.js
+++ 0x09-react_redux_connectors_and_providers~/task_0/dashboard/src/index.js
@@ -2,13 +2,25 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App/App.js";
 import BodySection from "./BodySection/BodySection";
+import { combineReducers, createStore } from "redux";
+import { uiReducer } from "./reducers/uiReducer.js";
+import { courseReducer } from "./reducers/courseReducer.js";
+import { notificationReducer } from "./reducers/notificationReducer.js"
+import { Provider } from "react-redux"
 const dom = document.querySelector("#root");
 const root = createRoot(dom);
-
+const rootReducer = combineReducers({
+  uiReducer,
+  // notificationReducer,
+  // courseReducer,
+})
+const store = createStore(rootReducer);
 const logOut = () => {};
 root.render(
   <div>
     {/* <App Isloggedin={false} logOut={logOut} /> */}
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </div>
 );
